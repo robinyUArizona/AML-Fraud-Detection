256f3546d18459ab3923df7594c11a675424bf32
robinyUArizona <robiny@arizona.edu>
2024-08-31 14:10:21 -0400

    Added data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -20,22 +20,22 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Entered the 'data ingestion' method or component")
+        logging.info(f"Entered the 'data ingestion' method or component")
         try:
             df = pd.read_csv("notebook\data\HI-Small_Trans.csv")
             # Take 50,000 sampples of the data
             df_sample = df.sample(n=50000, random_state=6)
-            logging.info("Read the dataset as DataFrame")
+            logging.info(f"Read the dataset as DataFrame")
             # create directory for raw data
             os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)), exist_ok=True)
             # save raw data
             df_sample.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            logging.info("Train Test split initiated")
+            logging.info(f"Train Test split initiated")
             train_set, test_set = train_test_split(df_sample, test_size=0.2, random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-            logging.info("Ingestion of the data completed")
+            logging.info(f"Ingestion of the data completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -47,4 +47,10 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    from src.components.data_transformation import DataTransformation
+    from src.components.data_transformation import DataTransformationConfig
+    data_transformation =  DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+
