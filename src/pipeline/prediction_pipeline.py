614d9c6f290124f36d775c57ec61065b6ea2fd76
robinyUArizona <robiny@arizona.edu>
2024-09-05 10:37:32 -0400

    Added Docker file & updated prediction_pipeline.py file

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 
 from src.exception import CustomerException
@@ -12,8 +13,8 @@ class PredictionPipeline:
 
     def predict(self, features):
         try: 
-            model_path = "artifacts\model.pkl"
-            preprocessor_path = "artifacts\preprocessor.pkl"
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
             data_scaled = preprocessor.transform(features)
