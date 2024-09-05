9c60c214e6ea23d4a499da2089aac599c3bf7fec
robinyUArizona <robiny@arizona.edu>
2024-09-05 19:50:56 -0400

    udpated

--- app.py
+++ app.py
@@ -30,5 +30,6 @@ def predict_datapoint():
         results = predict_pipeline.predict(pred_df)
         return render_template("home.html", results=results[0])
     
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
