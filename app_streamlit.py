db2e02e306cf52533d9a0145c7683874b4f71499
robinyUArizona <robiny@arizona.edu>
2024-09-08 07:16:55 -0400

    streamlit app updated

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,62 +2,69 @@ import streamlit as st
 import dill
 from src.pipeline.prediction_pipeline import CustomData, PredictionPipeline
 from src.exception import CustomerException
+from src.logger import logging
 import pandas as pd
 
+def main():
+    logging.info(f"Starting Streamlit App")
+    st.write(
+    """
+    # Anti-Money Laundering (AML) Fraud Detection
+            
+    This app predicts the given transaction is *fraud* or *not fraud*
+    """
+    )
+    st.write("-------")
 
-st.write(
-"""
-# Anti-Money Laundering (AML) Fraud Detection
-         
-This app predicts the given transaction is *fraud* or *not fraud*
-"""
-)
-st.write("-------")
-
-# Sidebar
-# Header of specify Input Parameters
-st.sidebar.header("Specify Input Features")
+    # Sidebar
+    # Header of specify Input Parameters
+    st.sidebar.header("Specify Input Features")
 
 
-def user_input_features():
-    data = CustomData(
-            from_bank = st.sidebar.number_input("from_bank"),
-            account = st.sidebar.text_input("account"),
-            to_bank = st.sidebar.number_input("to_bank"),
-            account_1 = st.sidebar.text_input("account_1"),
-            amount_received =  st.sidebar.number_input("amount_received"),
-            receiving_currency = st.sidebar.text_input("receiving_currency"),
-            payment_format = st.sidebar.text_input("payment_format")
-        )
-    features_df = data.get_data_as_DataFrame()
-    # print(features_df)
-    return features_df
+    def user_input_features():
+        data = CustomData(
+                from_bank = st.sidebar.number_input("from_bank"),
+                account = st.sidebar.text_input("account"),
+                to_bank = st.sidebar.number_input("to_bank"),
+                account_1 = st.sidebar.text_input("account_1"),
+                amount_received =  st.sidebar.number_input("amount_received"),
+                receiving_currency = st.sidebar.text_input("receiving_currency"),
+                payment_format = st.sidebar.text_input("payment_format")
+            )
+        features_df = data.get_data_as_DataFrame()
+        # print(features_df)
+        return features_df
 
-df = user_input_features()
+    df = user_input_features()
 
-# Print specified input parameters
-st.header("Specified Input parameters")
-st.write(df)
-st.write("----------")
+    # Print specified input parameters
+    st.header("Specified Input parameters")
+    st.write(df)
+    st.write("----------")
 
-# Call best model
-predict_pipeline = PredictionPipeline()
-# Apply best model to make prediction
-prediction = predict_pipeline.predict(df)
-prediction_proba = predict_pipeline.predict_proba(df)
+    # Call best model
+    predict_pipeline = PredictionPipeline()
+    # Apply best model to make prediction
+    prediction = predict_pipeline.predict(df)
+    prediction_proba = predict_pipeline.predict_proba(df)
 
-st.subheader('Fraud detector class labels')
-class_labels_df = pd.DataFrame({"Not Fraud" : [0], "Fraud" : [1]})
-class_labels_df.index = ["Class labels"]
-st.write(class_labels_df.T)
+    st.subheader('Fraud detector class labels')
+    class_labels_df = pd.DataFrame({"Not Fraud" : [0], "Fraud" : [1]})
+    class_labels_df.index = ["Class labels"]
+    st.write(class_labels_df.T)
 
-st.header("Prediction of a given transaction")
-st.write(prediction)
-st.write("-------")
+    st.header("Prediction of a given transaction")
+    st.write(prediction)
+    st.write("-------")
 
-st.header("Prediction probabilities of a given transaction")
-st.write(prediction_proba)
-st.write("-------")
+    st.header("Prediction probabilities of a given transaction")
+    st.write(prediction_proba)
+    st.write("-------")
+    logging.info(f"Streamlit app execution completed")
+
+
+if __name__ == "__main__":
+    main()
 
 # Explaining the model's predictions using SHAP values
 # https://github.com/slundberg/shap
@@ -85,5 +92,3 @@ st.write("-------")
 #         st.write("This is column 2")
     
 
-# if __name__ == "__main__":
-#     main()
